0d9f5d221f7cbd191219fff5cb19def993fb51fd
Kirill Varlamov <k.varlamov@brain4net.com>
2018-03-15 21:48:45 +0300

    LTY-22 Fix styling issues and add comments

--- contracts/LongevityToken.sol
+++ contracts/LongevityToken.sol
@@ -7,10 +7,14 @@ import "./MultiMintableToken.sol";
 import "./MultiOwnable.sol";
 
 
+/**
+ * @title token of Longevity project
+ * @dev LongevityToken is a top-level contract which defines standard token getters:
+ * name, symbol and decimals
+ */
 contract LongevityToken is ERC827Token, MultiMintableToken, BurnableToken {
     string public name = "Longevity";
     string public symbol = "LTY";
     uint8 public decimals = 2;
-
     function LongevityToken() public MultiMintableToken() {}
 }
